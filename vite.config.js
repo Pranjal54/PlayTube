cb497536e1e17dfa70a7efca6c4c548d2184bf50
Pranjal Mishra <97456066+Pranjal54@users.noreply.github.com>
2025-03-11 10:57:22 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://playtube-sq4g.onrender.com/api/v1",
+    },
+  },
+});
